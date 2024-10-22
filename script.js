4cced821eec371a5317f6491b6d36865034e6fe1
myousuffs <mailyousuffsend@gmail.com>
2024-10-22 22:08:29 +0530

    project updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document
   .getElementById("outside-border")
   .addEventListener("click", function () {
-    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
   });
 
 //Todo: Form to get info JS.
